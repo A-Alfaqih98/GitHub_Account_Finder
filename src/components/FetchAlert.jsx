62febc06e78f350527274bec293d3d893eceb659
Abdulrahman Alfaqih <Abdulrahman.alfaqih98@gmail.com>
2022-01-22 21:58:55 +0300

    Add files via upload

---
+++ src/components/FetchAlert.jsx
@@ -0,0 +1,11 @@
+function FetchAlert() {
+  return (
+    <div className="container">
+      <p className=" alert alert-info p-0 text-center">
+        Only 10 fetches per minute can be made
+      </p>
+    </div>
+  );
+}
+
+export default FetchAlert;
